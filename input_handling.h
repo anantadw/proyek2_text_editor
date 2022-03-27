42b82764fe46b85fc4d413249f1a34f182d82b32
arimaulanapolban <ari.maulana.tif21@polban.ac.id>
2022-03-28 00:40:57 +0700

    header deleted 1 karakter terakhir

--- input_handling.h
+++ input_handling.h
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <cstring>
+#include <windows.h>
 
 /* menginput karakter ke array */
 void inputCharacter(char [][MAX_COLUMN + 2], int &, int &, char);
+
+/* menghapus 1 karakter terakhir dari array */
+void deleteCharacter(char [][MAX_COLUMN + 2], int &, int &);
