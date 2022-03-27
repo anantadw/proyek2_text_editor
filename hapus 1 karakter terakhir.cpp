2e431fae1e3d70a681243d5090629a2c31ef7922
arimaulanapolban <ari.maulana.tif21@polban.ac.id>
2022-03-28 00:31:42 +0700

    hapus 1 karakter terakhir

---
+++ hapus 1 karakter terakhir.cpp
@@ -0,0 +1,15 @@
+#include "input_handling.h"
+
+/* menghapus 1 karakter terakhir dari array */
+void deleteCharacter(char teks[][MAX_COLUMN + 2], int &row, int &column, int &cursor_x) {
+	teks[row][column] = 0x00;
+	column -= 1;
+	printf("\b \b");
+	if (column < 0) {
+		column = 0;
+	}
+	cursor_x -= 1;
+	if (cursor_x < 0) {
+		cursor_x = 0;
+	}
+}
